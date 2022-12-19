581edbcffa50b47071b69c96345034fc7f7e7d2c
Mateus <mateussb31@yahoo.com.br>
2022-12-19 18:50:20 -0300

    Grid layout

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
 <script setup lang="ts">
 import HeaderVue from './components/Header.vue';
 import PlayerVue from './components/Player.vue';
-
+import MainVue from './components/Main.vue';
 </script>
 
 <template>
-
-  <main>
-    <PlayerVue></PlayerVue>
+  <header>
     <HeaderVue></HeaderVue>
-
+  </header>
+  <main>
+    <MainVue></MainVue>
   </main>
+  <aside><PlayerVue></PlayerVue></aside>
 </template>
 
 <style scoped>
---
+++ src/components/Main.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>blablabla</p>
+    </div>
+</template>
